6704d0d9fe1608ddf97ce36a46710fb729e1573f
Zoltanbi <zoltanbiro001@gmail.com>
2022-11-26 15:03:58 -0800

    Login page design

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -2,7 +2,24 @@ import "./login.scss"
 
 const Login = () => {
   return (
-    <div className="login">Login</div>
+    <div className="login">
+        <div className="card">
+            <div className="left">
+                <h1>Hello World.</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident omnis aliquam ab quas ea dolor distinctio natus harum aliquid maiores. Odio pariatur officia mollitia molestias iure a quisquam excepturi necessitatibus amet eaque?</p>
+                <span>Don't you have an account?</span>
+                <button>Register</button>
+            </div>
+            <div className="right">
+                <h1>Login</h1>
+                <form>
+                    <input type="text" placeholder="Username" />
+                    <input type="password" placeholder="Password" />
+                    <button>Login</button>
+                </form>
+            </div>
+        </div>
+    </div>
   )
 }
 
